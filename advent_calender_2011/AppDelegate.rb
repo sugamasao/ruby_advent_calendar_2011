5062cac4d2fd1b129868084ad4916ff2db4feca9
sugamasao <sugamasao@gmail.com>
2011-12-24 22:29:57 +0900

    Initial Commit

---
+++ advent_calender_2011/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  advent_calender_2011
+#
+#  Created by  on 11/12/24.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+	attr_accessor :window
+	def applicationDidFinishLaunching(a_notification)
+		# Insert code here to initialize your application
+	end
+end
+
